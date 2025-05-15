06e782df2b203493da7ecf8aadbf4d114621d4ef
TheRaccoonGod <justint5262@gmail.com>
2025-05-14 21:16:43 -0700

    Added basic pdf extractor

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,14 @@
 import numpy as np
 import pandas as pd
 import json
+import fitz #(pymupdf)
 
+pdf_path = ""
+
+doc = fitz.open(pdf_path)
+
+for page_num, page in enumerate(doc, start=1):
+    text = page.get_text()
+    print(text)
+    
+doc.close()
